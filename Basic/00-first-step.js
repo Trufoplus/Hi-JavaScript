79660ab2a5cd23c57ac7c074025a51bcd24b25cf
Daniel Galvan <dani_tenerife9@hotmail.com>
2024-07-13 12:18:43 +0100

    Primeros pasos en JS

---
+++ Basic/00-first-step.js
@@ -0,0 +1,37 @@
+/*
+
+PRIMEROS PASOS EN JS:
+
+- Para usar JS en tu PC es necesario instalar Node.js.
+- Node.js es un entorno de ejecución de JavaScript que te permite 
+  ejecutar JavaScript en tu PC fuera del navegador.
+
+- Una vez instalado, puedes ver qué versión tienes instalada escribiendo
+  en la terminal "node -v".
+
+- Los archivos en JS se definen en kebab-case, con guiones en los espacios y en
+  minúscula, por ejemplo: "00-hello-world.js".
+
+- "console.log('Hello World')" es un comando básico que imprime un mensaje por 
+  la consola.
+
+Instrucciones Adicionales:
+
+1. Instalar Node.js: Puedes descargar Node.js desde [aquí](https://nodejs.org/).
+
+2. Crear un archivo JavaScript:
+   - Abre tu editor de texto favorito (por ejemplo, Visual Studio Code).
+   - Crea un nuevo archivo llamado `00-hello-world.js`.
+   - Copia y pega el código de ejemplo en el archivo.
+
+3. Ejecutar el archivo JavaScript:
+   - Abre la terminal en la carpeta donde guardaste el archivo.
+   - Escribe `node 00-hello-world.js` y presiona Enter.
+   - Deberías ver el mensaje "Hello World" impreso en la consola.
+
+*/
+
+// Ejemplo de un archivo JS básico:
+
+// 00-hello-world.js
+console.log('Hello World');
