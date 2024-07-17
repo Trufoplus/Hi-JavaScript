dce64cf8ff89e06e90aec418a09b2826fd1d3016
Daniel Galvan <dani_tenerife9@hotmail.com>
2024-07-17 21:01:45 +0100

    tipos de datos

--- Basic/02-datatype.js
+++ Basic/02-datatype.js
@@ -1,55 +1,56 @@
 // TIPOS DE DATOS PRIMITIVOS
-// Son los datos en los que se fundamenta nuestro codigo
-// Los datos primitivos: son los datos basicos e inmutables
-// Hay 7 tipos de datos primitivo:
+// Son los datos en los que se fundamenta nuestro código
+// Los datos primitivos son los datos básicos e inmutables
+// Hay 7 tipos de datos primitivos:
+
 // String (cadena de texto)
-let cadena = 'Hola mundo'
-let alias = 'Dhanny'
-let email = 'dani@gmail.com'
+let cadena = 'Hola mundo';
+let alias = 'Dhanny';
+let email = 'dani@gmail.com';
 
 // Números (number)
-let age = 35 //Entero
-let height = 1.77 // Decimal
+let age = 35; // Entero
+let height = 1.77; // Decimal
 
 // Booleanos (boolean)
-let isStudent = false
-let isTeacher = true
+let isStudent = false;
+let isTeacher = true;
 
 // Undefined
-// Es una variable sin nungun valor asociado.
-// Nos imprimira por consola (undefined = indefinido).
-let undefinedValue
-console.log(undefinedValue)
+// Es una variable sin ningún valor asociado.
+// Nos imprimirá por consola (undefined = indefinido).
+let undefinedValue;
+console.log(undefinedValue); // undefined
 
 // Null
-let nullValue = null
+// Es un valor que representa la ausencia intencional de cualquier valor.
+let nullValue = null;
 
-//Symbol
-// Son valores unicos que pueden ser identificaddos como  identificadores de propiedades
-// para los futuros objetos y evitar colisiones entre otra objetos
-let mySymbol = Symbol('mysymbol')
+// Symbol
+// Son valores únicos que pueden ser usados como identificadores de propiedades
+// para los futuros objetos y evitar colisiones entre otras propiedades.
+let mySymbol = Symbol('mysymbol');
 
-//BigInt
-// son numeros enteros extremadamente grandes
-let myBigInt = BigInt(78364798162397486123978469128364)
-let myBightInt2 = 94167781326478613289468177238n
-
+// BigInt
+// Son números enteros extremadamente grandes
+let myBigInt = BigInt(78364798162397486123978469128364);
+let myBigInt2 = 94167781326478613289468177238n;
 
 // Mostramos los tipos de datos
-console.log(typeof cadena) //imprimira string
-console.log(typeof email) //imprimira string
-console.log(typeof alias) //imprimira string
+console.log(typeof cadena); // imprimirá string
+console.log(typeof email); // imprimirá string
+console.log(typeof alias); // imprimirá string
 
-console.log(typeof age) //imprimira number
-console.log(typeof height) //imprimira number
+console.log(typeof age); // imprimirá number
+console.log(typeof height); // imprimirá number
 
-console.log(typeof isStudent) //imprimira boolean
-console.log(typeof isTeacher) //imprimira boolean
+console.log(typeof isStudent); // imprimirá boolean
+console.log(typeof isTeacher); // imprimirá boolean
 
-console.log(typeof undefinedValue) //imprimira undefined
-console.log(typeof nullValue) //imprimira object
+console.log(typeof undefinedValue); // imprimirá undefined
+console.log(typeof nullValue); // imprimirá object (esto es una peculiaridad de JavaScript)
 
-console.log(typeof mySymbol) //imprimira symbol
+console.log(typeof mySymbol); // imprimirá symbol
 
-console.log(typeof myBigInt) //imprimira bigint
-console.log(typeof myBightInt2) //imprimira bigint
+console.log(typeof myBigInt); // imprimirá bigint
+console.log(typeof myBigInt2); // imprimirá bigint
