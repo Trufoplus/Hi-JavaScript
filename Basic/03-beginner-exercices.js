a79d4dee00b55f86bfab71e819ef918c53e9eab7
Daniel Galvan <dani_tenerife9@hotmail.com>
2024-07-18 18:22:07 +0100

    ejercicios

--- Basic/03-beginner-exercices.js
+++ Basic/03-beginner-exercices.js
@@ -1,23 +1,20 @@
-// 1. Escribe un comentario de una linea
 
-// 2. Escribe un comentario en varias lineas
+// 1. Escribe un comentario en una línea
 
+// 2. Escribe un comentario en varias líneas
 
-// 3. Declara variables con valores asoiciados a todos los datos de tipo primitivos
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
-
-
-// 4. Imprime por consola el valor de todoas las variables
+// 4. Imprime por consola el valor de todas las variables
 
 // 5. Imprime por consola el tipo de todas las variables
 
-// 6. A continuacion, modifica los valores de las variavbles por otros del mismo tipo
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
-// 7. A continuacion, modifica los valores de las variables por otros de distinto tipo
-
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
-// 9. A continuacion, modifica los valores de las constantes
+// 9. A continuación, modifica los valores de las constantes
 
-//
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
