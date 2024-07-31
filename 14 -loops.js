8a9c2986b0fe38d3ea5ff9f09fa98c052ea41f9a
Daniel Galvan <dani_tenerife9@hotmail.com>
2024-07-31 20:55:48 +0100

    bucles

--- 14 -loops.js
+++ 14 -loops.js
@@ -1,42 +1,42 @@
 ////////// LOOPS o BUCLES
 /*
-Un bucle es una estructura de control, para repetir un bloque de codigo
-mientras una condicion sea verdadera.
+Un bucle es una estructura de control para repetir un bloque de código
+mientras una condición sea verdadera.
  */
 
 
 // FOR
 /* 
-se ejecuta mientra la condicion sea true si es false deja de ejecutar
-Usamos un for (en el ejemplo abajo) y entre parentecis ponemos la condicion, declaramos una
-variable le damos un valor, y mientras sea menor que 5 sera true, la 
-tercera condicion se ejecuta despues de cada vuelta del bucle y lo que hace
-es sumar 1 a la variabble i .
+Se ejecuta mientras la condición sea true; si es false, deja de ejecutar.
+Usamos un for (en el ejemplo abajo) y entre paréntesis ponemos la condición. 
+Declaramos una variable, le damos un valor, y mientras sea menor que 5 será true. 
+La tercera condición se ejecuta después de cada vuelta del bucle y lo que hace 
+es sumar 1 a la variable i.
 */
 for (let i = 0; i < 5; i++) {
-    console.log('Hola mundo')
+    console.log('Hola mundo');
 } 
 
 
 // Para iterar sobre un listado, hacemos lo siguiente:
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
 
 for (let i = 0; i < numbers.length; i++) {
-    console.log(`Elemento: ${numbers[i]}`)
+    console.log(`Elemento: ${numbers[i]}`);
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // WHILE
-// en un while la variable se declare fuera, dentro solo la condicion
-let i = 0
+// En un while, la variable se declara fuera, dentro solo la condición.
+let i = 0;
 while (i < 5) {
-    console.log(`Hola ${i}`)
-    i++
+    console.log(`Hola ${i}`);
+    i++;
 }
 
 /*
-creamos un bucle infinito asi:
+Creamos un bucle infinito así:
 
 while(true){
 
@@ -46,61 +46,58 @@ while(true){
 
 ///////////////////////////////////////////////////////////////////////////////
 // DO WHILE
-//minimo una vez se ejecutara el bloque de codigo
+// Mínimo una vez se ejecutará el bloque de código.
 
-i = 6
+i = 6;
 do {
-    console.log(`Hola ${i}`)
-    i++
-} while (i < 5) // a pesar de que i es mayor a 5 se ejecutara al menos una vez
+    console.log(`Hola ${i}`);
+    i++;
+} while (i < 5); // A pesar de que i es mayor a 5, se ejecutará al menos una vez.
 
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // FOR OF
-// es para recorrer algo que sea interable (una estructura de datos)
-myArray = new Array(1, 2, 3, 'string', false)
-mySet = new Set(['frikone', 'dani', 'daniel', 35, 'dani@gmail.com'])
-myMap = new Map([
+// Es para recorrer algo que sea iterable (una estructura de datos).
+let myArray = [1, 2, 3, 'string', false];
+let mySet = new Set(['frikone', 'dani', 'daniel', 35, 'dani@gmail.com']);
+let myMap = new Map([
     ['name', 'dani'],
     ['email', 'dani@gmail.com'],
     ['age', 35]
-])
+]);
 
 for (let valor of myArray) {
-    console.log(valor)
-} //nos devolvera cada dato de nuestro array/lista
+    console.log(valor);
+} // Nos devolverá cada dato de nuestro array/lista.
 
 for (let valor of mySet) {
-    console.log(valor)
+    console.log(valor);
+}
+
+for (let [clave, valor] of myMap) {
+    console.log(`${clave}: ${valor}`);
 } 
 
-
-for (let valor of myMap) {
-    console.log(valor)
-} 
-
-
-
-myString = 'Hola JavaScript'
+let myString = 'Hola JavaScript';
 for (let valor of myString) {
-    console.log(valor)
-} // iterara sobre cada letra del array
+    console.log(valor);
+} // Iterará sobre cada letra del string.
 
 
 
 ///////////////////////////////////////////////////////////////////////////////
 /// BUENAS PRACTICAS
 /*
-+ Revisar que la condicion no cree un bucle infinito
-+ Emplear break y continue para evitar bucle infinito
++ Revisar que la condición no cree un bucle infinito.
++ Emplear break y continue para evitar bucles infinitos.
 */
 
 for (let i = 0; i < 10; i++) {
-    if (i == 5) {
-        continue
-    } else if (i == 7) {
-        break
+    if (i === 5) {
+        continue;
+    } else if (i === 7) {
+        break;
     }
-    console.log(`Hola mundo ${i}`)
-} 
+    console.log(`Hola mundo ${i}`);
+}
